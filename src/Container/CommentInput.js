aeb364a28af4bab2b91ed8ec870e960ded80130e
pp <405788914@qq.com>
2018-02-07 21:05:53 +0800

    init

--- src/Container/CommentInput.js
+++ src/Container/CommentInput.js
@@ -1,10 +1,17 @@
 import React ,{Component} from 'react'
 import PropTypes from 'prop-types'
+import { WingBlank,List, InputItem } from 'antd-mobile'
 
 class CommentInput extends Component{
   render(){
     return(
-      <div>test</div>
+      <div>
+        <WingBlank>
+        <List>
+          <InputItem/>
+        </List>
+        </WingBlank>
+      </div>
     )
   }
 }
